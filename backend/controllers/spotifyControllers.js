0b182ffa2390d2f9abc28ba537bb5d8016cf46c5
MARUT <marutpanwar@gmail.com>
2026-03-21 23:53:45 +0530

    user data extraction with access token

--- backend/controllers/spotifyControllers.js
+++ backend/controllers/spotifyControllers.js
@@ -1,17 +1,74 @@
 import dotenv from 'dotenv';
-dotenv.config();
-const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+import axios from "axios";
+import {asynchandler} from "../middleware/asynchandler.js";
 
-export const redirectToSpotify = (req, res) => {
+dotenv.config();
+export const redirectToSpotify = asynchandler(
+  (req, res) => {
     const scope = "playlist-read-private playlist-modify-private playlist-modify-public"
     const authurl = "https://accounts.spotify.com/authorize?"+
       new URLSearchParams({
         response_type:"code",
-        client_id:CLIENT_ID,
-            scope:scope,
-            redirect_uri:REDIRECT_URI,
+        client_id:process.env.SPOTIFY_CLIENT_ID,
+        scope:scope,
+        redirect_uri:process.env.SPOTIFY_REDIRECT_URI,
       })
-
+      console.log("authurl :",authurl);
       res.redirect(authurl);
 }
+)
+
+
+
+export const spotifyCallback = asynchandler(
+  async (req,res)=>{
+    const code = req.query.code;
+    if(!code){
+      res.status(401);
+      throw new Error("Authorization code missing");
+    }
+
+    const tokenResponse = await axios.post("https://accounts.spotify.com/api/token",new URLSearchParams({
+      code:code,
+      redirect_uri:process.env.SPOTIFY_REDIRECT_URI,
+      grant_type: "authorization_code"
+    }).toString(),
+    {
+      headers: {
+      "Content-type":"application/x-www-form-urlencoded",
+      Authorization : "Basic " +
+      Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64"),
+    },}
+     );
+
+     const {access_token,refresh_token,expires_in} = tokenResponse.data;
+      res.status(200).json({
+      success:true,
+      access_token,
+      refresh_token,
+      expires_in
+     });
+
+
+
+     const userProfile = await axios.get("https://api.spotify.com/v1/me",{
+      headers:{
+        Authorization: `Bearer ${access_token}`,
+      }
+     });
+
+     console.log(userProfile.data);
+    //  res.status(200).json({
+    //   success:true,
+    //   access_token,
+    //   refresh_token,
+    //   expires_in
+    //  });
+
+  }
+);
+
+
+
+
+
--- backend/routes/spotifyRoutes.js
+++ backend/routes/spotifyRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { redirectToSpotify } from "../controllers/spotifyControllers.js";
+import { redirectToSpotify, spotifyCallback } from "../controllers/spotifyControllers.js";
 
 
 
 router.get("/login" , redirectToSpotify);
+router.get("/callback",spotifyCallback);
 
 export default router;
